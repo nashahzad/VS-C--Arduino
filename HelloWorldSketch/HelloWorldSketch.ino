39c2b55c8dfcff535b4eeda244e22cb11158ce0b
nashahzad <nauman.a.shahzad@gmail.com>
2016-01-04 22:02:58 -0500

    Initial commit to repo

---
+++ HelloWorldSketch/HelloWorldSketch.ino
@@ -0,0 +1,19 @@
+/*
+ Name:		HelloWorldSketch.ino
+ Created:	1/4/2016 9:37:36 PM
+ Author:	Nomi
+*/
+
+// the setup function runs once when you press reset or power the board
+#include "LED13.h"
+
+LED13 object;
+
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+	object.blink(1000);
+}
---
+++ HelloWorldSketch/LED13.h
@@ -0,0 +1,23 @@
+// LED13.h
+
+#ifndef _LED13_h
+#define _LED13_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+class LED13
+{
+ public:
+	 LED13();
+	 ~LED13();
+	 void on();
+	 void off();
+	 void blink(int time);
+};
+
+#endif
+
